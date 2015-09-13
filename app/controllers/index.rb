f9789172ab809d294617c72a4a75b259fe51e0bb
Stephanie Hutson <email": null>
2015-09-12 18:26:45 -0700

    make small changes to how the csv file collects data

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,39 @@
+## read
 get '/' do
   @ordered_workouts = Workout.order(:date)
   erb :index
+end
+
+get '/workouts' do
+  erb :index
+end
+
+get '/workouts/:id' do
+  #probably not something I want
+end
+
+#create
+get '/workouts/new' do
+
+end
+
+post '/workouts' do
+
+end
+
+#edit
+
+get '/workouts/:id/edit' do
+
+end
+
+post '/workouts/:id' do
+
+end
+
+#delete
+
+post '/workouts/:id' do
+  Workout.where(id: params[:id]).destroy
+  redirect '/'
 end
