52c04d7bada90e33fb0ca317419a89cddc058194
Stephanie Hutson <email": null>
2015-09-12 19:10:39 -0700

    log a workout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,42 @@
+## read
 get '/' do
+
   @ordered_workouts = Workout.order(:date)
   erb :index
+end
+
+get '/workouts' do
+  erb :index
+end
+get '/workouts/new' do
+  "Hello World"
+  erb :new_workout
+end
+
+get '/workouts/:id' do
+  #probably not something I want
+end
+
+#create
+
+post '/workouts' do
+  Workout.create(params)
+  redirect '/workouts'
+end
+
+#edit
+
+get '/workouts/:id/edit' do
+
+end
+
+post '/workouts/:id' do
+
+end
+
+#delete
+
+post '/workouts/:id' do
+  Workout.where(id: params[:id]).destroy
+  redirect '/'
 end
