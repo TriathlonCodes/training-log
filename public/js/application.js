7cf907bb7ba2daf2b998927596b2b79694c30108
Stephanie Hutson <email": null>
2015-09-15 23:56:10 -0700

    change id on css to log not full log for selector

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-  $("#full_log > tbody > tr").on("click", function() {
+  $("#log > tbody > tr").on("click", function() {
     $(this).css({
       "background-color" : "lightblue"
-
     })
     $(this).addClass("selected")
   })
